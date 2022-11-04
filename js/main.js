ec4f2edfb3c46c1ca1d4f3564b9067ab56925a95
KIM MINSU <112890661+chunjaeilu@users.noreply.github.com>
2022-11-04 13:14:53 +0900

    헤더, 메인배너, 인트로덕션 구현
    - 헤더, 메인배너, 인트로덕션 구현(1차작업)
    - 메인배너 이미지 수정(좌우 검은색 그라데이션으로 꽉찬 효과)
    - js, 메인배너 이미지 전환

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 /* main.js */
+$(function () {
+  let bgArr = [
+    "images/main-banner-1.jpg",
+    "images/main-banner-2.jpg",
+    "images/main-banner-3.jpg",
+  ];
+  /* 스크립트에서 경로를 불러올 때는 js파일 폴더나 css파일 폴더가 기준이 아닌, HTML문서 기준으로 찾는다.
+  DOM 조작은 attribute(인라인속성)을 변경하는 것이기 때문 */
+
+  let bg_img_num = 0;
+
+  setInterval(function () {
+    bg_img_num++;
+
+    if (bg_img_num >= bgArr.length) {
+      bg_img_num = 0;
+    }
+    $("#main-banner .container").css({
+      "background-image": `url(${bgArr[bg_img_num]})`,
+    });
+  }, 5000);
+});
